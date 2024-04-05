9a2fafe1a6e8880bc27982d70c6186f262bea3c3
Sébastien Avit <sebastien.avit98gmail.com>
2024-04-05 12:52:36 +0200

    End

--- js/main.js
+++ js/main.js
@@ -14,41 +14,50 @@ menuBurger.addEventListener("click", () => {
 // Class Modal
 const popUpOverlay = document.querySelector(".popup_modale");
 const popUpContainer = document.querySelector(".popup_container");
+
 // Link Contact Menu Header
 const menuContactHeader = document.querySelector(
-    ".header__menu__desktop .page-item-83 a"
+    ".header__menu__desktop .menu-item-23 a"
 );
+
 // Link Contact Menu Mobile
 const menuContactMobile = document.querySelector(
-    ".header__menu__mobile .page-item-83 a"
+    ".header__menu__mobile .menu-item-23 a"
 );
+
 // function Open Modale Form
-function toggleModal() {
+function toggleModal(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
     popUpOverlay.style.display = "flex";
     void popUpOverlay.offsetWidth;
     popUpOverlay.classList.add("is-visible");
     popUpContainer.classList.toggle("is-display");
 }
+
 // Open Modale Form
 [menuContactHeader, menuContactMobile].forEach(el => {
     el.addEventListener("click", toggleModal);
 });
+
 // Open Modale Btn Contact Single Page Photo
-const contactSinglePageModal = document.querySelector(".post_contact_link");
+const contactSinglePageModal = document.querySelector(".post_contact_link a");
 if (contactSinglePageModal) {
     contactSinglePageModal.addEventListener("click", toggleModal);
 }
+
 // Function Close Modale Form
 function closeModal() {
     popUpOverlay.classList.remove("is-visible");
     popUpContainer.classList.remove("is-display");
 }
+
 // Listener event close modal click outside
 popUpOverlay.addEventListener("click", event => {
     if (event.target === popUpOverlay) {
         closeModal();
     }
 });
+
 // Listener event close modal press key escape
 document.addEventListener("keydown", event => {
     if (
@@ -59,6 +68,7 @@ document.addEventListener("keydown", event => {
         closeModal();
     }
 });
+
 // Listener for transition end to hide the modal
 popUpOverlay.addEventListener("transitionend", function (event) {
     if (
@@ -84,16 +94,27 @@ function prefillReferenceInput() {
     }
 }
 
+console.log(reference)
+
+
 // Listener for the click event on the contact button
 const contactButton = document.querySelector('.post_contact_link');
 if (contactButton) {
-    contactButton.addEventListener('click', prefillReferenceInput);
+    contactButton.addEventListener('click', handleContactButtonClick);
+}
+
+// Function to handle click on the contact button
+function handleContactButtonClick(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    toggleModal(event); // Pass the event to toggleModal function to handle it properly
+    prefillReferenceInput()
 }
 
 // Function to handle click on contact menu links
 function handleContactMenuClick(event) {
     event.preventDefault(); // Prevent the default action of the link
-    toggleModal(); // Open the modal
+    toggleModal(event); // Pass the event to toggleModal function to handle it properly
+    prefillReferenceInput()
 }
 
 // Attach click event listeners to both contact menu links
@@ -105,9 +126,11 @@ if (menuContactMobile) {
     menuContactMobile.addEventListener("click", handleContactMenuClick);
 }
 
+
+
 document.addEventListener("DOMContentLoaded", function() {
     // Trouver l'élément "Accueil" du menu
-    const accueilLink = document.querySelector(".header__menu__desktop .page-item-77 a");
+    const accueilLink = document.querySelector(".header__menu__desktop .menu-item-104 a");
 
     // Trouver l'URL du lien du logo
     const logoLink = document.querySelector(".header_logo_link").getAttribute("href");
@@ -117,3 +140,5 @@ document.addEventListener("DOMContentLoaded", function() {
         accueilLink.setAttribute("href", logoLink);
     }
 });
+
+console.log("h")
